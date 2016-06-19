31357a44de82a2f6783ffe1c4b192d22dd708ffe
Jimmy Paul <jppay@users.noreply.github.com>
2016-06-19 13:23:32 +0530

    XML Parsing

---
+++ XMLParseHelp.java
@@ -0,0 +1,120 @@
+
+
+public class XMLParseHelp {
+	
+	private String escapeAmpersands(String unescapedMessageText) {
+		/*
+		 * This escaping is a bit tortured. Apparently there is a mixture of escaped characters and 
+		 * un-escaped ampersands being passed in. So any un-escaped ampersands are escaped and then  
+		 * the process reversed for already-escaped characters. StringUtil doesn't have any regex-matching
+		 * capability so it is necessary to do it in this manner.
+		 */
+		
+		//first the un-escape ampersands and less than are escaped
+		String partiallyEscapedMessageText = StringUtil.replace(unescapedMessageText, "&", "&amp;", false);
+		partiallyEscapedMessageText = StringUtil.replace(partiallyEscapedMessageText, "<", "&lt;", false);
+        
+        //then the process reversed for already-escaped characters
+        partiallyEscapedMessageText = StringUtil.replace(partiallyEscapedMessageText, "&amp;quot;", "&quot;", false);
+        partiallyEscapedMessageText = StringUtil.replace(partiallyEscapedMessageText, "&amp;apos;", "&apos;", false);
+        partiallyEscapedMessageText = StringUtil.replace(partiallyEscapedMessageText, "&amp;lt;"  , "&lt;"  , false);
+        partiallyEscapedMessageText = StringUtil.replace(partiallyEscapedMessageText, "&amp;gt;"  , "&gt;"  , false);
+        String escapedMessageText  = StringUtil.replace(partiallyEscapedMessageText, "&amp;amp;" , "&amp;" , false);
+        
+        //Escape '<' character available in real-time data
+		escapedMessageText = parseLessThan(escapedMessageText, "<", "&lt;");
+        
+        if(log.isDebugEnabled())
+        {
+            log.debug("Any ampersands and < in the messageText have been escaped to '&amp;' and '&lt;'");
+            // dump the Text message
+            log.debug("Escaped message is now:\n<" + escapedMessageText + ">");
+        }
+		return escapedMessageText;
+	}
+	
+	
+	  /*
+	   * Escape less than character in the real-time data
+	   * <P>The following characters are replaced with corresponding character entities : 
+	   * <tr><th> Character </th><th> Encoding </th></tr>
+	   * <tr><td> < </td><td> &lt; </td></tr>
+	   * <P>
+	   */
+	private String parseLessThan(
+		String text,
+		String oldSubString,
+		String newSubString) 
+	{
+		String searchString = text;
+		String searchPattern = oldSubString;
+		int index = searchString.indexOf("<", 1);
+		StringBuffer buf = new StringBuffer(text);
+		while (index != -1) {
+				
+				if((buf.charAt(index - 1) != '>') && (buf.charAt(index + 1) != '/'))
+				{
+					buf.replace(index, index + searchPattern.length(), newSubString);
+				}
+	
+				text = buf.toString();
+				searchString = text;
+				index = searchString.indexOf(searchPattern, index + newSubString.length());
+		}
+	
+		return text;
+	}
+	
+}
+
+class StringUtil {
+	
+	/**
+	 * Replaces all occurences of oldSubString in string with the newSubString.
+	 * If ignoreCase replacements is asked, the search for oldSubString does
+	 * a case insensitive search for the oldSubString.
+	 * 
+	 * @param text the text for which the replacement has to be performed
+	 * @param oldSubString the text which has to be replaced
+	 * @param newSubString the text which has to replace the oldSubString
+	 * occurrences
+	 * @param ignoreCase <code>true</code> if the replacement should be case
+	 * insensitive
+	 * @return the text with all occurrences of oldSubString replaced with
+	 * newSubString
+	 */
+	public static String replace(
+		String text,
+		String oldSubString,
+		String newSubString,
+		boolean ignoreCase) {
+			
+		if (!ignoreCase){
+			return replace(text, oldSubString, newSubString,-1);
+		}else{
+			String searchString = text;
+			String searchPattern = oldSubString;
+			if (ignoreCase) {
+				searchString = searchString.toLowerCase();
+				searchPattern = searchPattern.toLowerCase();
+			}
+	
+			int index = searchString.indexOf(searchPattern);
+			while (index != -1) {
+				StringBuffer buf = new StringBuffer(text);
+				buf.replace(index, index + searchPattern.length(), newSubString);
+	
+				text = buf.toString();
+				searchString = text;
+				if (ignoreCase)
+					searchString = searchString.toLowerCase();
+	
+				index = searchString.indexOf(searchPattern, index + newSubString.length());
+			}
+	
+			return text;
+		}
+	}
+	
+	
+}
