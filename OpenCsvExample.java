a3aeb088a8b92cbf063ae7e9db87617c71ce13cb
Jimmy Paul <jppay@users.noreply.github.com>
2016-09-20 17:58:55 +0530

    Create OpenCsvExample.java

---
+++ OpenCsvExample.java
@@ -0,0 +1,17 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
+
+# The following two imports should be used for Java 1.5 and lower
+#import au.com.bytecode.opencsv.CSVReader;
+#import au.com.bytecode.opencsv.CSVWriter;
+
+# Import for Java 1.6 and above
+import com.opencsv.CSVReader;
+import com.opencsv.CSVWriter;
+
+public class OpenCsvExample {
+  
+
+}
